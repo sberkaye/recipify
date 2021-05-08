57d99415035d6208b6d1f553b005a01c2e9d353d
sberkaye <berkayergin99@gmail.com>
2021-05-08 04:11:32 +0300

    Created the header section and started using Material UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Container, Typography, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import headerMedium from './images/header-bg-medium.jpg';
+// import headerSmall from "./images/header-bg-small.jpg";
 
-const App = () => <div>SA</div>;
+const useStyles = makeStyles((theme) => ({
+  headerContainer: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${headerMedium})`,
+    height: 300,
+    padding: theme.spacing(4),
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <Container className={classes.headerContainer} maxWidth="xl">
+        <Typography variant="h1" align="center">
+          Recipify
+        </Typography>
+      </Container>
+      <div>SEARCH</div>
+    </>
+  );
+};
 
 export default App;
